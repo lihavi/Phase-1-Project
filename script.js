0cf6bb3423210944dd9a594e6e3bbda97a84b990
vivianne <viviannjoroge91@gmail.com>
2022-12-16 12:44:33 +0300

    This commit will add the updated script.js file and style.css

--- script.js
+++ script.js
@@ -4,42 +4,49 @@ document.addEventListener("DOMContentLoaded",() => {
         e.preventDefault();
         let form =e.target;
     });
-    
-})
-const train_num=5600
-const trainName="madaraka express"
-const departTime=0800
-const trains= [
-]
-let train = {train_num,trainName,departTime}
+});
 
+
+const train_num = 12235
+const trainName = "Dibrugarh - New Delhi Rajdhani Express"
+const departTime = 1925
+const trains = [
+    {train_num:12236, trainName: "New Delhi - Dibrugarh Rajdhani Express",  departTime: 0925,},
+  {train_num:12301, trainName: "Howrah - New Delhi Rajdhani Express (via Gaya)",  departTime:1655 ,},
+   {train_num:12302, trainName: "New Delhi - Howrah Rajdhani Express (Via Gaya)", departTime:1655,},
+   {train_num:12305, trainName: "	Howrah - New Delhi Rajdhani Express (via Patna)",  departTime:1405,},
+   {train_num:12306, trainName: "	New Delhi - Howrah Rajdhani Express (via Patna)",  departTime:1655,}  
+]
+let train = {train_num, trainName, departTime}
+trains.push(train);
 console.log(trains);
 
-const input=document.getElementById('my Form').addEventListener("click", clickAlert);
-function clickAlert(){
-    alert('Your details have been submitted succesfully')
-}
-document.getElementById("demo")
-function myFunction(){
-    document.getElementById("demo").innerHTML ="YOU CLICKED ME!";
 
+const input = document.getElementById('myForm').addEventListener("click", clickAlert);
+
+function clickAlert() {
+  alert('Your details has been submitted successfully');
 }
+
+document.getElementById("demo").addEventListener("click", myFunction);
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+
+
 function renderTrainRow(train){
     console.log(train)
-    let row=document.createElement('tr');
+    let row =document.createElement('tr');
     row.innerHTML=`
-    <td>${train.train_num}</<td>
+    <td>${train.train_num}</td>
     <td>${train.name}</td>
-    <td>${train.data.departTime}</td>
     <td>${train.data.arriveTime}</td>
+    <td>${train.data.departTime}</td>
     <td>${train.train_from}</td>
     <td>${train.train_to}</td>
     `;
-
-document.querySelector('.table').appendChild(row)
+    document.querySelector('.table').appendChild(row)
 }
-
-
 function getAllTrains(){const options = {
 	method: 'POST',
 	headers: {
@@ -52,14 +59,42 @@ function getAllTrains(){const options = {
 
 fetch('https://trains.p.rapidapi.com/', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => {
+        response.forEach(train => renderTrainRow(train));
+        console.log(response)
+    })
 	.catch(err => console.error(err));
- }
-//     function initialize(){
-//         getAllTrains()
-//     }
-//    initialize();
-    
+}
 
+var tickets = [];
 
-        
+document.getElementById("ticket-form").addEventListener("submit", submitOrder);
+function submitOrder(event){
+    event.preventDefault();
+    var trainNo = document.getElementById('trainNumber').value;
+    var trainName = document.getElementById('trainName').value;
+    var departure = document.getElementById('departure').value;
+    var ticket = {
+        trainNo,
+        trainName,
+        departure
+    };
+    tickets.push(ticket);
+    console.log('tickets', tickets)
+}
+
+function renderTickets(ticket){
+    console.log(ticket)
+    let row =document.createElement('tr');
+    row.innerHTML=`
+    <td>${tickets.trainNo}</td>
+    <td>${ticket.trainName}</td>
+    <td>${ticket.departure}</td>
+    `;
+    document.querySelector('.table').appendChild(row)
+}
+
+// function initialize(){
+//     getAllTrains()
+// }
+// initialize();
