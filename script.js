c83c242bfceb47b835d075cad8a4bdc467f1ad9d
vivianne <viviannjoroge91@gmail.com>
2022-12-16 11:57:06 +0300

    This commit will make changes to the files

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
 document.addEventListener("DOMContentLoaded",() => {
     getAllTrains();
-    document.querySelector("#table-order").addEventListener("submit",(e) => {
+    document.querySelector("#table-order").addEventListener("submit", (e) => {
         e.preventDefault();
-        let form=e.target;
-    })
+        let form =e.target;
+    });
+    
 })
 const train_num=5600
 const trainName="madaraka express"
 const departTime=0800
 const trains= [
-    {train_num:5601,trainName:"DMU",departTime:0940,},
-    {train_num:5602,trainName:"Transit Shed",departTime:1200,},
-    {train_num:5603,trainName:"NCR",departTime:0900,},
-    {train_num:5604,trainName:"SGR",departTime:2000,},
 ]
+let train = {train_num,trainName,departTime}
+
+console.log(trains);
+
+const input=document.getElementById('my Form').addEventListener("click", clickAlert);
+function clickAlert(){
+    alert('Your details have been submitted succesfully')
+}
+document.getElementById("demo")
+function myFunction(){
+    document.getElementById("demo").innerHTML ="YOU CLICKED ME!";
+
+}
+function renderTrainRow(train){
+    console.log(train)
+    let row=document.createElement('tr');
+    row.innerHTML=`
+    <td>${train.train_num}</<td>
+    <td>${train.name}</td>
+    <td>${train.data.departTime}</td>
+    <td>${train.data.arriveTime}</td>
+    <td>${train.train_from}</td>
+    <td>${train.train_to}</td>
+    `;
+
+document.querySelector('.table').appendChild(row)
+}
+
+
+function getAllTrains(){const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '91915021edmsha0b4e2c95975759p1b9176jsnffcc33e7e69f',
+		'X-RapidAPI-Host': 'trains.p.rapidapi.com'
+	},
+	body: '{"search":"Rajdhani"}'
+};
+
+fetch('https://trains.p.rapidapi.com/', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+ }
+//     function initialize(){
+//         getAllTrains()
+//     }
+//    initialize();
+    
+
+
+        
